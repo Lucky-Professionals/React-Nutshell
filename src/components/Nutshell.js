b1ad374eff9282d868b05743375caf9cac6c506a
Andrew Herring <andrewoherring@gmail.com>
2018-11-28 10:30:08 -0600

    Here we go again bois

---
+++ src/components/Nutshell.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+import AppViews from "./AppViews"
+
+import "./Nutshell.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+export default class Nutshell extends Component {
+    render() {
+        return (
+            <React.Fragment>
+              
+            </React.Fragment>
+        )
+    }
+}
+
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Nutshell from './components/Nutshell';
+import { BrowserRouter as Router } from "react-router-dom"
+
+ReactDOM.render(
+<Router>
+  <Nutshell />
+</Router>,
+document.getElementById('root'))
+
+
