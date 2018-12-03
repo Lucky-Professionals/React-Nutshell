0d4b2b869cae73e0e8ecb79f1fd7854a8474a0c7
Andrew Herring <andrewoherring@gmail.com>
2018-12-03 11:46:08 -0600

    profile update' :

--- src/components/profile/profileEdit.js
+++ src/components/profile/profileEdit.js
@@ -2,86 +2,91 @@ import React, { Component } from "react"
 import "./profile.css"
 
 export default class ProfileEdit extends Component {
-    state = {
-      
-    }
-
-    handleFieldChange = evt => {
-        const stateToChange = {}
-        stateToChange[evt.target.id] = evt.target.value
-        this.setState(stateToChange)
-    }
-
-    componentDidMount() {
-      const profile = this.props.profiles.find(a => a.id === parseInt(this.props.match.params.profileId))
-      this.setState({...profile})
+  state = {
+    name: "",
+    location: "",
+    favFood: "",
+    favColor: "",
+    quote: "",
+    id: null,
   }
 
-    newProfile = (evt) => {
-      evt.preventDefault()
+  handleFieldChange = evt => {
+    const stateToChange = {}
+    stateToChange[evt.target.id] = evt.target.value
+    this.setState(stateToChange)
+  }
 
-      let editedProfile = {
-        name: this.state.name,
-        location: this.state.location,
-        favFood: this.state.favFood,
-        favColor: this.state.favColor,
-        quote: this.state.quote,
-        id: this.state.id
-      }
-      this.props.editProfile(this.state.id, editedProfile)
+  componentDidMount() {
+    const editProfile = this.props.profiles.find(a => a.id === parseInt(this.props.match.params.profilesId))
+    this.setState({ ...editProfile })
+  }
+
+  newProfile = (evt) => {
+    evt.preventDefault()
+
+    let editedProfile = {
+      name: this.state.name,
+      location: this.state.location,
+      favFood: this.state.favFood,
+      favColor: this.state.favColor,
+      quote: this.state.quote,
+      id: this.state.id
+    }
+    this.props.editProfile(this.state.id, editedProfile)
       .then(() => {
         this.props.history.push("/profile")
       })
-    }
+  }
 
-            
-    
 
-    render() {
-        return (
-            <React.Fragment>
-                <form className="profileForm list">
-                    <div className="form-group">
-                        <label htmlFor="profileName">Profile Name</label>
-                        <input type="text" required
-                               className="form-control"
-                               onChange={this.handleFieldChange}
-                               id="name"
-                               placeholder={this.state.name} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="location">Location</label>
-                        <input type="text" required
-                               className="form-control"
-                               onChange={this.handleFieldChange}
-                               id="location" placeholder={this.state.location} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="favFood">Favorite Food</label>
-                        <input type="text" required
-                               className="form-control"
-                               onChange={this.handleFieldChange}
-                               id="favFood" placeholder={this.state.favFood} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="favColor">Favorite Color</label>
-                        <input type="text" required
-                               className="form-control"
-                               onChange={this.handleFieldChange}
-                               id="favColor" placeholder={this.state.favColor} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="quote">Like I always say...</label>
-                        <input type="text" required
-                               className="form-control"
-                               onChange={this.handleFieldChange}
-                               id="quote" placeholder={this.state.quote} />
-                    </div>
-                    
-                    <button type="submit" onClick={this.newProfile} className="btn btn-primary">Save Your Profile</button>
-                </form>
-            </React.Fragment>
-        )
-    }
+
+
+  render() {
+    return (
+      <React.Fragment>
+        <form className="profileForm list">
+          <div className="form-group">
+            <label htmlFor="profileName">Profile Name</label>
+            <input type="text" required
+              className="form-control"
+              onChange={this.handleFieldChange}
+              id="name"
+              placeholder={this.state.name} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="location">Location</label>
+            <input type="text" required
+              className="form-control"
+              onChange={this.handleFieldChange}
+              id="location" placeholder={this.state.location} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="favFood">Favorite Food</label>
+            <input type="text" required
+              className="form-control"
+              onChange={this.handleFieldChange}
+              id="favFood" placeholder={this.state.favFood} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="favColor">Favorite Color</label>
+            <input type="text" required
+              className="form-control"
+              onChange={this.handleFieldChange}
+              id="favColor" placeholder={this.state.favColor} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="quote">Like I always say...</label>
+            <input type="text" required
+              className="form-control"
+              onChange={this.handleFieldChange}
+              id="quote" placeholder={this.state.quote} />
+          </div>
+
+          <button type="submit" onClick={this.newProfile} className="btn btn-primary">Save Your Profile</button>
+        </form>
+      </React.Fragment>
+    )
+  }
 }
 
