6c0a898ef0e21518ff15a0acf46d8a453e01b98c
Andrew Herring <andrewoherring@gmail.com>
2018-12-04 10:14:22 -0600

    Profile detail cards

--- src/components/profile/profilePage.js
+++ src/components/profile/profilePage.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react"
+import React, { Component, Link } from "react"
 import { Image, Card, Button, Icon, Header } from 'semantic-ui-react'
-import rebel2 from "./images/rebel2.jpg"
+import joe from "./images/joe.jpg"
 import "./profile.css"
 
 export default class ProfilePage extends Component {
@@ -12,19 +12,19 @@ export default class ProfilePage extends Component {
         <Header color="blue" as='h2' icon textAlign='center'>
           <Icon name='user circle' />
           <Header.Content>User Profiles</Header.Content>
-        </Header>   
+        </Header>
         <div className="profileBtn">
-          <Button color="blue" fluid onClick={() => {this.props.history.push("/profile/new")}}>
+          <Button color="blue" fluid onClick={() => { this.props.history.push("/profile/new") }}>
             <Button.Content>Add Your Profile!</Button.Content>
-            </Button>
-        </div>     
+          </Button>
+        </div>
         <section className="cards">
           {
             this.props.profiles.map(profiles =>
               <div key={profiles.id} className="list">
                 <Card>
                   <div className="profileCards">
-                    <Image src={rebel2} />
+                    <Image src={joe} />
                     <Card.Description>
                       <Card.Header>{profiles.name}</Card.Header>
                       <Card.Meta>{profiles.location}</Card.Meta>
@@ -34,15 +34,22 @@ export default class ProfilePage extends Component {
                     <br></br>
                     <section>
 
+
+
                     </section>
                     <section>
                       <div className="edBtns">
                         <Button.Group>
                           <Button color="teal" onClick={() => this.props.history.push(`/profile/edit/${profiles.id}`)}
                             className="editBtn">Edit
-                     </Button>
-                          <Button.Or />>
-                    <Button color="red" onClick={() => this.props.deleteProfile("profiles", profiles.id)}
+                          </Button>
+                          <Button.Or />
+                          <Button color="blue" onClick={() => this.props.history.push(`/profile/detail/${profiles.id}`)}
+                            className="deleteBtn">
+                            Details
+                          </Button>
+                          <Button.Or />
+                          <Button color="red" onClick={() => this.props.deleteProfile("profiles", profiles.id)}
                             className="deleteBtn">
                             Remove
                      </Button>
