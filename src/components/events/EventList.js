620360bc6a7659a3bfeb81051463ac5597395495
Noah Woods <noahwoods@gmail.com>
2018-12-04 11:16:20 -0600

    final events

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -1,19 +1,45 @@
 
 import { Route, Redirect } from 'react-router-dom'
 import React, { Component } from 'react'
+import DataManager from '../../module/DataManager'
 import { Link } from "react-router-dom";
-import { Button, Icon, Header } from 'semantic-ui-react'
+import { Button, Icon, Header, Card} from 'semantic-ui-react'
 import "./events.css"
 export default class EventList extends Component {
+  credentials = JSON.parse(localStorage.getItem('credentials'))
+
+  state = {
+    events: []
+  }
+
+  deleteEvent = (events, id) => {
+    DataManager.delete("events", id)
+    .then(() => DataManager.getAllByUser("events", this.credentials.id))
+    .then(allEvents => this.setState({
+      events: allEvents
+    })
+    )
+  }
+
+  componentDidMount() {
+    const newState = {}
+    DataManager.getAll("events", this.credentials.id)
+      .then(allEvents => {
+        newState.events = allEvents
+      })
+      .then(() =>
+        this.setState(newState))
+  }
 
   render() {
     return (
       <React.Fragment>
-
-        <Header as='h2' className="center aligned icon" color="blue">
-          <Icon name='calendar alternate outline icon' />
-          <Header.Content>Events</Header.Content>
-        </Header>
+        <div className="eventsHeader">
+          <Header as='h2' className="center aligned icon" color="blue">
+            <Icon name='calendar alternate outline icon' />
+            <Header.Content>Events</Header.Content>
+          </Header>
+        </div>
         <div className="eventsButton">
           <Button type="button" className="ui labeled icon button"
             color="blue"
@@ -27,32 +53,33 @@ export default class EventList extends Component {
         </div>
         <section >
           {
-            this.props.events.map(events =>
-              <div className="center">
-
-                <div className="article list" key={events.id}>
-                  <div className="ui centered card">
-                    <div id="cardTextAlign" className="center aligned description">
-                      <h3 className="ui center aligned dividing header">{events.name}</h3><br></br>
-                      <div id="cardText" className="ui center aligned description">{events.date}<br></br>
-                        {events.location}</div><br></br>
-                      <div className="ui dividing header">
-                        <Button.Group size="mini">
-                          <Button
-                            color="teal"
-                            onClick={() => this.props.history.push(`/events/edit/${events.id}`)}
-                            className="event-edit-button">Edit
+            this.state.events.map(events =>
+              <div className="center" key={events.id}>
+                <div className="article list" >
+                  {/* <Card > */}
+                    <div id="eventCards" className="ui centered card">
+                      <div id="cardTextAlign" className="center aligned description">
+                        <h3 className="ui center aligned dividing header">{events.name}</h3><br></br>
+                        <div id="cardText" className="ui center aligned description">{events.date}<br></br>
+                          {events.location}</div><br></br>
+                        <div className="ui dividing header">
+                          <Button.Group size="mini">
+                            <Button
+                              color="teal"
+                              onClick={() => this.props.history.push(`/events/edit/${events.id}`)}
+                              className="event-edit-button">Edit
                 </Button>
-                          <Button.Or />
-                          <Button
-                            color="red"
-                            onClick={() => this.props.deleteEvent(events.id)}
-                            className="event-delete-button">Delete
+                            <Button.Or />
+                            <Button
+                              color="red"
+                              onClick={() => this.deleteEvent("events", events.id)}
+                              className="event-delete-button">Delete
                 </Button>
-                        </Button.Group>
+                          </Button.Group>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  {/* </Card> */}
                 </div>
               </div>
             )
