f9e4b9799f6f99c2a4dba3280723c954e262565f
buffard <samuelwebber19@gmail.com>
2018-12-04 10:27:22 -0600

    working authentication

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -4,6 +4,12 @@ import TodoForm from './TodoForm';
 import "./todos.css"
 
 class TodoList extends Component {
+
+  findUserId = () => {
+    return localStorage.getItem("credentials")
+}
+
+
   makeCompletedTodo = (id) => {
     const completedTodo = {
       completed: true
@@ -11,17 +17,22 @@ class TodoList extends Component {
     this.props.editTodo(id, completedTodo)
   }
   render() {
-    
+    const credentials = JSON.parse(localStorage.getItem('credentials'))
+
+    // Filter only todos that are false(uncompleted) and that have the correct user id
     const uncompletedTodos = this.props.todos.filter(todos =>
-      todos.completed=== false) || {}
+      todos.userId === credentials.id && todos.completed=== false) ||{}
 
 
     return (
       <React.Fragment>
+        {/* Todos Icon  */}
         <Header color="blue" as='h2' icon textAlign='center'>
           <Icon name='bell outline' />
           <Header.Content>To Do List</Header.Content>
         </Header> 
+
+        {/* Pulls our form first */}
         <TodoForm {...this.props}/>
         {/* individual list of tasks */}
         <section className="container">
